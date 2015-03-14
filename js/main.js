6d2dea37c11e57c574a2c562cbb39cb95bbb9204
aninder <aninder@gmail.com>
2015-03-14 22:31:08 +0530

    js hidden elements for day month , media query

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ var MONTH = new Date().getMonth();
 var DAY = 1;
 function setBodyHeader(dateObj) {
     var header = document.getElementsByTagName('header')[0];
-    header.innerHTML = "<h1>"+dateObj.getCurrentMonth+" "+dateObj.getCurrentYear+"</h1>";
+    header.innerHTML = "<h1>"+dateObj.getCurrentMonthName+" "+dateObj.getCurrentYear+"</h1>";
 }
 function createDom() {
     var ul = document.getElementsByClassName('days')[0];
@@ -15,13 +15,6 @@ function createDom() {
         //newNode.id='row'+i;
         for(var j = 0 ; j < 7 ; j++) {
             new_day = li_proto.cloneNode(true);
-
-//            obj = sheet.shift();
-//            new_day.firstElementChild.textContent=obj.date;
-//            if(!obj.currentMonth) {
-//                new_day.className = new_day.className + " other-month";
-//            }
-
             newNode.appendChild(new_day);
         }
         ul_parent.appendChild(newNode);
@@ -34,31 +27,51 @@ function clearClassNames() {
         nodeList[i].className = "day";   
     }
 }
+function clearHiddenDivs() {
+    var hiddenDivs = document.querySelectorAll(".hidden");
+    for(var i=0 ; i < hiddenDivs.length ; i++) {
+        var parent = hiddenDivs[i].parentNode;
+        parent.removeChild(hiddenDivs[i]);
+    }
+}
 function addDates(sheet) {
     clearClassNames();
+    clearHiddenDivs();
     var nodeList = document.querySelectorAll(".date");
     for(var i = 0 ; i < nodeList.length ; i++) {
         //add date from sheet array
-        obj = sheet.shift();
+        var obj = sheet.shift();
         nodeList[i].textContent = obj.date;
         //add other-month class for dates not in current month
         if(!obj.currentMonth) {
             nodeList[i].parentNode.className = nodeList[i].parentNode.className + " other-month";
         }
+        //create hidden element to store month name and day name to display in mobile mode
+        //only for first time
+            var li = nodeList[i].parentNode
+            create_hidden_div(obj.monthName,li,"month");
+            create_hidden_div(obj.day,li,"dayname");
     }
 };
+function create_hidden_div(value,li,klass) {
+        div = document.createElement('div',li);
+        div.className="hidden "+klass;
+        div.textContent=value;
+        li.appendChild(div);
+}
 function DateObj(date) {
     if( this.constructor !== DateObj) {
         return new DateObj(date);
     }
     else {
         var dateArr = date.toString().split(" ");
-        var curr_month = dateArr[1];
+        var curr_month_name = dateArr[1];
         var curr_year = dateArr[3];
         var last_day_of_last_month = new Date(date.getFullYear(),date.getMonth(),0).getDate();
         var last_day_of_current_month = 1;
+        //calculate last date of current_month
         while(true) {
-            tempdate=new Date(date.getFullYear(),date.getMonth(),last_day_of_current_month+1);
+            var tempdate=new Date(date.getFullYear(),date.getMonth(),last_day_of_current_month+1);
             if(tempdate.getDate() === 1) {
                 break;
             }
@@ -66,31 +79,43 @@ function DateObj(date) {
                 last_day_of_current_month+=1;
             }
         }
+        //get last month name
+        var last_month_name = new Date(date.getFullYear(),date.getMonth(),0).toString().split(" ")[1];
+        //get next month name
+        var next_month_name = new Date(date.getFullYear(), date.getMonth(), last_day_of_current_month+1).toString().split(" ")[1];
     }
     return {
       getFirstDayOfCurrentMonth:date.getDay(),
-      getCurrentMonth:curr_month,
+      getCurrentMonthName:curr_month_name,
       getCurrentYear:curr_year,
+      getCurrentMonth:date.getMonth(),
       getLastDateOfCurrentMonth:last_day_of_current_month,
-      getLastDateOfLastMonth:last_day_of_last_month
+      getLastDateOfLastMonth:last_day_of_last_month,
+      getLastMonthame:last_month_name,
+      getNextMonthName:next_month_name
     }
 };
 function createSheet(dateObj) {
     var sheet = [];
     //add all dates for current month
     for(var i=1; i<=dateObj.getLastDateOfCurrentMonth; i++) {
-        sheet.push({date:i,currentMonth:true});
+    //save the day
+    var day = new Date(dateObj.getCurrentYear,dateObj.getCurrentMonth,i).toString().split(" ")[0];   
+        
+    sheet.push({date:i,currentMonth:true,monthName:dateObj.getCurrentMonthName,day:day});
     }
     //add dates from last month if first day of current month not on sunday
     var counter = dateObj.getLastDateOfLastMonth;
     for( var j=0 ; j < dateObj.getFirstDayOfCurrentMonth ; j++) {
-        sheet.unshift({date:counter,currentMonth:false})
+    var day = new Date(dateObj.getCurrentYear,dateObj.getCurrentMonth - 1,counter).toString().split(" ")[0];            
+    sheet.unshift({date:counter,currentMonth:false,monthName:dateObj.getLastMonthame,day:day})
         counter-=1;
     }
     //add dates from next month till sheet length not equal to 42
     counter = 1;
     while(sheet.length != 42) {
-        sheet.push({date:counter,currentMonth:false});
+    var day = new Date(dateObj.getCurrentYear,dateObj.getCurrentMonth + 1,counter).toString().split(" ")[0];            
+    sheet.push({date:counter,currentMonth:false,monthName:dateObj.getNextMonthName,day:day});
         counter+=1;
     }
     return sheet;
@@ -101,12 +126,14 @@ function addDatesinDom(dateObj) {
     setBodyHeader(dateObj);
     addDates(sheet);
 }
+
+
 window.addEventListener('load', function(){
     createDom();
     var dateObj = DateObj(new Date(YEAR, MONTH, DAY));
     addDatesinDom(dateObj);
     
-    document.querySelector('input[type=month]').addEventListener('change',          function(event){
+    document.querySelector('input[type=month]').addEventListener('change', function(event){
         yearmonth = event.srcElement.value.split("-");
         year = yearmonth[0];
         //input type date returns 1 index month and date object
